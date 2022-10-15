59bd1e2cbd77631120b4fa74d1e40af543d4554a
root <root@mail.salenty.com>
2022-10-15 19:32:54 +0000

    Revert "Some trash on server"
    This reverts commit 3f25766d14f2ccf8da086c8919af5bc2e6e087f9.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI() #(middleware=middleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("app.app:app", host="0.0.0.0", log_level="info", port=8080, reload=True)
--- backend/start_daemon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp polushack.service  /etc/systemd/system/polushack.service
-systemctl daemon-reload
-systemctl restart polushack.service
-systemctl enable polushack.service   #добавить в автозагрузку
