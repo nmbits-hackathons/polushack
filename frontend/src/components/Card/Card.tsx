314333188acb83d7abd22fdad030fc16ff7e9087
Ivan Sosnovich <isosnovich@yandex.ru>
2022-10-15 22:44:28 +0300

    добавил информацию о заявке и иконки де машина и куда едет

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,8 +1,20 @@
-import React from "react"
-import { Tag } from "antd"
+import React, { useState } from "react"
+import { Tag, Button } from "antd"
+import {
+    EditOutlined,
+    DeleteOutlined,
+    DownOutlined,
+    UpOutlined
+} from "@ant-design/icons"
+import Maps from "./components/Maps"
 import { getColorForStatus } from "helpers/getColorForStatus"
 import { getTextForStatus } from "helpers/getTextForStatus"
 import "./Card.css"
+
+interface TableDescription {
+    title: string
+    value: string
+}
 
 export interface CardProps {
     id: string
@@ -15,6 +27,10 @@ export interface CardProps {
     secondInformation: string
     secondIcon: React.FC
     status: string
+    tableDescription?: TableDescription[]
+    forFullDescription?: string
+    transportCoords: string
+    placeCoords: string
 }
 
 const Card = ({
@@ -25,40 +41,99 @@ const Card = ({
     firstInformation,
     firstIcon: FirstIcon,
     secondInformation,
-    secondIcon,
-    status
+    secondIcon: SecondIcon,
+    status,
+    tableDescription,
+    forFullDescription,
+    transportCoords,
+    placeCoords
 }: CardProps) => {
+    const [open, setOpen] = useState(false)
+    const handleOpenFullInfo = () => {
+        setOpen((prev) => !prev)
+    }
     return (
         <div className="card-wrapper">
-            <div className="card-information">
-                {titleImage && (
-                    <img
-                        src={titleImage}
-                        alt="картинка описания"
-                        className="information-image"
+            <div className="card-wrapper-main">
+                <div className="card-information">
+                    {titleImage && (
+                        <img
+                            src={titleImage}
+                            alt="картинка описания"
+                            className="information-image"
+                        />
+                    )}
+                    <div className="information-main">
+                        <p className="information-id">{id}</p>
+                        <h2 className="name-title">{title}</h2>
+                        <p className="information-description">{description}</p>
+                    </div>
+                </div>
+                <div className="card-time">
+                    <FirstIcon />
+                    <div className="card-time-info">{firstInformation}</div>
+                </div>
+                <div className="card-date">
+                    <SecondIcon />
+                    <div className="card-time-info">{secondInformation}</div>
+                </div>
+                <div className="card-status">
+                    <Tag color={getColorForStatus(status)}>
+                        {getTextForStatus(status)}
+                    </Tag>
+                </div>
+                <div className="card-action">
+                    <Button
+                        type="text"
+                        shape="circle"
+                        icon={<EditOutlined />}
+                        size="middle"
                     />
-                )}
-                <div className="information-main">
-                    <p className="information-id">{id}</p>
-                    <h2 className="name-title">{title}</h2>
-                    <p className="information-description">{description}</p>
+                    <Button
+                        type="text"
+                        shape="circle"
+                        icon={<DeleteOutlined />}
+                        size="middle"
+                    />
+                </div>
+                <div className="card-drop">
+                    <Button
+                        type="text"
+                        shape="circle"
+                        icon={open ? <UpOutlined /> : <DownOutlined />}
+                        size="middle"
+                        onClick={handleOpenFullInfo}
+                    />
                 </div>
             </div>
-            <div className="card-time">
-                <FirstIcon />
-                <div className="card-time-info">{firstInformation}</div>
-            </div>
-            <div className="card-date">
-                <FirstIcon />
-                <div className="card-time-info">{firstInformation}</div>
-            </div>
-            <div className="card-status">
-                <Tag color={getColorForStatus(status)}>
-                    {getTextForStatus(status)}
-                </Tag>
-            </div>
-            <div className="card-action"></div>
-            <div className="card-drop"></div>
+            {open && (
+                <div className="card-wrapper-full">
+                    <div className="wrapper-full-information">
+                        {tableDescription &&
+                            tableDescription.map((values) => {
+                                return (
+                                    <div className="full-information-table">
+                                        <h2 className="information-table-title">
+                                            {values.title}
+                                        </h2>
+                                        <p className="information-table-description">
+                                            {values.value}
+                                        </p>
+                                    </div>
+                                )
+                            })}
+                        <div className="full-information-description">
+                            <h2 className="information-description-title">
+                                Описание заявки
+                            </h2>
+                            <p className="information-description-info">
+                                {forFullDescription}
+                            </p>
+                        </div>
+                    </div>
+                    <Maps {...{ transportCoords, placeCoords }} />
+                </div>
+            )}
         </div>
     )
 }
--- frontend/src/redux/application/interface.ts
+++ frontend/src/redux/application/interface.ts
@@ -1,36 +1,37 @@
-import {TypeTransport} from "redux/transport";
+import { TypeTransport } from "redux/transport"
 
 export interface Application {
-    title: string;
-    description: string;
+    title: string
+    description: string
     type: TypeTransport
-    speed: number,
-    power: number,
-    operating_weight: number,
-    unloading_height: number,
-    creator: string,
-    time_start: string,
-    time_end: string,
-    priority: PriorityApplication,
-    to_place: string,
-    vin: string,
-    from_place: string | null,
-    distance: string | null,
-    average_time: string | null,
-    status: StatusApplication,
+    speed: number
+    power: number
+    operating_weight: number
+    unloading_height: number
+    creator: string
+    time_start: string
+    time_end: string
+    priority: PriorityApplication
+    to_place: string
+    vin: string
+    from_place: string | null
+    distance: string | null
+    average_time: string | null
+    status: StatusApplication
     id: string
+    current_place: string
 }
 
 export interface ApplicationState {
-    applications: Application[];
-    counts: number;
-    isLoading: boolean;
-    error?: string | null | unknown;
+    applications: Application[]
+    counts: number
+    isLoading: boolean
+    error?: string | null | unknown
 }
 
 export interface CreateApplicationDto {
-    title: string;
-    description: string;
+    title: string
+    description: string
     type: TypeTransport
     speed: number
     power: number
@@ -43,7 +44,12 @@ export interface CreateApplicationDto {
     to_place: string
 }
 
-export type StatusApplication = "confirmable" | "cancelled" | "progress" | "pending" | "active"
+export type StatusApplication =
+    | "confirmable"
+    | "cancelled"
+    | "progress"
+    | "pending"
+    | "active"
 
 // confirmable - заявка которую выполнили успешно
 // cancelled - заявка которую  отменили
